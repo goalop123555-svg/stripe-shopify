aa7d009a8ef7907107e2f051074cda3d87c65cfe
goalop123555-svg <goalop123555@gmail.com>
2025-10-08 23:48:40 +0300

    Fix: make api a regular folder

---
+++ api/create-payment-intent.js
@@ -0,0 +1,20 @@
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: 1000,
+        currency: 'usd',
+      });
+      res.status(200).json({ clientSecret: paymentIntent.client_secret });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
